988cf38c64caffd2ee9f6b40d2fe43abc29b9ed3
Celso <celsorico@gmail.comcd mnt/c/git config --global user.email celsorico@gmail.com>
2025-06-04 22:57:06 -0300

    js

---
+++ node-server/index.js
@@ -0,0 +1,19 @@
+console.log('Meu Primeiro JavaScript');
+
+/* Criação de Variaveis */
+var nome;
+var idade;
+var altura;
+var sexo;
+var e_programador;
+var peso;
+
+/* Atribuição de informação */
+nome = 'Celso Bastos'; // String
+idade = 1; // Number
+altura = 1_000.75; // Number
+sexo = 'M'; //String
+e_programador = false; // Boolean {true / false}
+peso = null;
+
+console.log(altura)
