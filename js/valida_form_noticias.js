54d956f01d36c360ed10a7b92ca73bec1e2ef4a6
Celso <celsorico@gmail.comcd mnt/c/git config --global user.email celsorico@gmail.com>
2025-06-05 22:15:34 -0300

    js

--- js/valida_form_noticias.js
+++ js/valida_form_noticias.js
@@ -1,1 +1,11 @@
-console.log("Meu JS no FrontEnd")
+console.log("Meu JS no FrontEnd");
+var email;
+var botao;
+
+botao = document.querySelector('.btn');
+botao.addEventListener('click', () => {
+    
+    email = document.querySelector('.email').value;
+    console.log(email);
+
+});
